866cd7daebd6f85ba371b9fb50a485ae24b6e46c
Anushk Jain <anushk2201@gmail.com>
2024-01-28 15:38:18 +0530

    feat: implemented main pages and routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-const App = () => {
-	
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 
+const App = () => {
 	return (
-		<div>
-			<h1>Worldwise</h1>
-			
-		</div>
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Homepage />} />
+				<Route path='product' element={<Product />} />
+				<Route path='pricing' element={<Pricing />} />
+				<Route path='*' element={<PageNotFound />} />
+			</Routes>
+		</BrowserRouter>
 	);
 };
 
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,7 @@
+const PageNotFound = () => {
+	return <div>
+        <h1>Not Found :( </h1>
+    </div>;
+};
+
+export default PageNotFound;
